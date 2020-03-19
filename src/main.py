ee39f8d8ae7a342cc6e07c08b3b07bf55a3a05e5
Néstor Arellano <asoretzu@gmail.com>
2020-03-19 05:47:14 -0600

    Decoded the message.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,22 @@
 # Resolve the problem!!
 
+import re
+
 
 def run():
-    # Start coding here
+    text = ""
+    letters = "[a-z]"
+    message = ""
+
+    with open("encoded.txt", mode="r", encoding="utf-8") as f:
+        for line in f:
+            text += line
+    
+    for letter in text:
+        if re.match(letters, letter):
+            message += letter
+    
+    print(message)
 
 
 if __name__ == '__main__':
